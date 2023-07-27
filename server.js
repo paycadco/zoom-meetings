3e7c439b1855530688da1bfb2ffdbe1d48f6a72d
Kato Drake Smith <ryansmith7119@gmail.com>
2023-07-27 15:41:06 +0300

    v2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const server = require("https").Server(app);
 const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
 const { ExpressPeerServer } = require("peer");
